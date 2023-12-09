f757be39b6d7be760d7a2a4801aaa185382f9c9c
jiangren-zaizai <2394276262@qq.com>
2023-12-09 22:27:36 +0800

    完善tab页面中

--- src/views/admin/tab.jsx
+++ src/views/admin/tab.jsx
@@ -8,22 +8,60 @@ import Transition from '@/components/Transition'
 import { useToggle, useMount } from 'ahooks'
 import { useState } from 'react'
 import http from '@/utlis/http'
+import { useEffect } from 'react'
 
 export default () => {
   const [isDelBut, { toggle }] = useToggle()
   const [checkboxs, setCheckoxs] = useState([])
+  const [removeTab, setRemoveTab] = useState([])
+  const [val, setval] = useState('')
 
   useMount(() => {
     http
-      .get('/getjson', { ph: 'tabs.json' })
-      .then(res => setCheckoxs(res))
+      .get('/json-get', { ph: 'tabs.json' })
+      .then((res) => setCheckoxs(res))
       .catch(() => {
         window.$message.error('获取便签失败')
       })
   })
 
-  const onChange = checkedValues => {
-    console.log('checked = ', checkedValues)
+  const onChange = (checkedValues) => {
+    setRemoveTab([...removeTab, checkedValues])
+  }
+
+  useEffect(() => {
+    if (removeTab.length > 0 && !isDelBut) {
+      toggle()
+    }
+  }, [removeTab.length])
+
+  const addTab = async () => {
+    await http
+      .post('/json-set', {
+        ph: 'tabs.json',
+        data: removeTab
+      })
+      .catch((e) => {
+        window.$message.error('添加标签失败')
+        return Promise.reject(e)
+      })
+    setCheckoxs([...checkboxs, val])
+  }
+
+  const removeTabClick =async ()=>{
+    const modalRes = await window.$modal.confirm({
+      okText: '删除',
+      okType: 'danger',
+      maskClosable: true,
+      centered: true,
+      cancelText: '取消',
+      title: '删除记录类型',
+      content: '是否将（删除）'
+    })
+    if(!modalRes){
+      setRemoveTab([])
+      setCheckoxs([])
+    }
   }
 
   return (
@@ -41,9 +79,16 @@ export default () => {
           </div>
         }
       >
-        <Space size={[0, 8]} wrap className='mt-10'>
-          {checkboxs.map(v => (
-            <Checkbox onChange={() => onChange(v)} key={v}>
+        <Space
+          size={[0, 8]}
+          wrap
+          className='mt-10'
+        >
+          {checkboxs.map((v) => (
+            <Checkbox
+              onChange={() => onChange(v)}
+              key={v}
+            >
               <Tag color={randomHexColor()}>{v}</Tag>
             </Checkbox>
           ))}
@@ -53,8 +98,20 @@ export default () => {
           labelAlign='right'
         >
           <Space.Compact className='w-100'>
-            <Input placeholder='标签名' />
-            <Button type='primary' icon={<CuIcon icon='add' size={16} />}>
+            <Input
+              placeholder='标签名'
+              onChange={(e) => setval(e.target.value)}
+            />
+            <Button
+              type='primary'
+              icon={
+                <CuIcon
+                  icon='add'
+                  size={16}
+                />
+              }
+              onClick={addTab}
+            >
               提交
             </Button>
           </Space.Compact>
@@ -62,8 +119,17 @@ export default () => {
 
         <Transition show={isDelBut}>
           {isDelBut ? (
-            <Button danger type='primary' block className='mt-10'>
-              <CuIcon icon='delete' size={16} />
+            <Button
+              danger
+              type='primary'
+              block
+              className='mt-10'
+              onClick={removeTabClick}
+            >
+              <CuIcon
+                icon='delete'
+                size={16}
+              />
             </Button>
           ) : (
             <></>
