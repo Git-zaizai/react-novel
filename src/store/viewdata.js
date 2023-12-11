705362491ebd3258eebb4ee8c30f983f1f4dbc3f
jiangren-zaizai <2394276262@qq.com>
2023-12-11 17:22:43 +0800

    重构了好多

---
+++ src/store/viewdata.js
@@ -0,0 +1,39 @@
+import { useMount } from 'ahooks'
+import { createGlobalStore } from 'hox'
+import { useState } from 'react'
+import http from '@/utlis/http'
+import { randomHexColor } from '@/utlis/themeColor'
+import { useToggle } from 'ahooks'
+
+const [viewdata, getViewData] = createGlobalStore(() => {
+  const [recordtypes, setRecordtype] = useState([])
+  const [tabs, setTabs] = useState([])
+
+  const initHttp = async () => {
+    try {
+      const response = await Promise.all([
+        http.get('/json-get', { ph: 'Recordtype.json' }),
+        http.get('/json-get', { ph: 'tabs.json' })
+      ])
+      const tabsdata = response.map((mv) =>
+        mv.map((mmv) => ({
+          tab: mmv,
+          color: randomHexColor()
+        }))
+      )
+      setRecordtype(tabsdata[0])
+      setTabs(tabsdata[1])
+    } catch {
+      window.$message && window.$message.error('获取类型标签失败')
+    }
+  }
+
+  useMount(() => {
+    initHttp()
+  })
+  return { recordtypes, setRecordtype, tabs, setTabs, initHttp }
+})
+
+export const useViewDataStore = viewdata
+
+export const getViewDataStore = getViewData
